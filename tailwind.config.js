76bf182c53086ddc8047c3d743b41c4f6baddfe7
Jeferson Mejicanos <alexandermejicanos57@gmail.com>
2024-04-07 22:13:43 -0600

    Challenge finish whit Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      "Green": "hsl(75, 94%, 57%)",
+      "White": "hsl(0, 0%, 100%)",
+      "Grey": "hsl(0, 0%, 20%)",
+      "Dark-Grey": "hsl(0, 0%, 12%)",
+      "Off-Black": "hsl(0, 0%, 8%)"
+    },
+    fontFamily: {
+      'Inter': ['Inter']
+    }
+  },
+  plugins: [],
+}
